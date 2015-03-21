ea54506281c14623bf394f4582822b81b833a498
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-03-21 17:56:51 +0300

    hw06 finished
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- hw06.hs
+++ hw06.hs
@@ -8,28 +8,30 @@ import Test.HUnit
 data Complex = Complex { real :: Double, im :: Double } deriving (Show, Eq)
 
 fromDouble :: Double -> Complex
-fromDouble = undefined
+fromDouble d = Complex { real = d, im = 0 }
 
 -- Мнимая единица
 i :: Complex
-i = undefined
+i = Complex { real = 0, im = 1 }
 
 infixl 6 +., -.
 (+.) :: Complex -> Complex -> Complex
-(+.) = undefined
+(+.) (Complex a1 b1) (Complex a2 b2) = Complex (a1 + a2) (b1 + b2)
 
 (-.) :: Complex -> Complex -> Complex
-(-.) = undefined
+(-.) (Complex a1 b1) (Complex a2 b2) = Complex (a1 - a2) (b1 - b2)
 
 infixl 7 *., /.
 (*.) :: Complex -> Complex -> Complex
-(*.) = undefined
+(*.) (Complex a1 b1) (Complex a2 b2) = Complex (a1 * a2 - b1 * b2) (a1 * b2 + a2 * b1)
 
 (/.) :: Complex -> Complex -> Complex
-(/.) = undefined
+(/.) (Complex a1 b1) (Complex a2 0) = Complex (a1 / a2) (b1 / a2)
+(/.) a b = (a *. (conj b)) /. (fromDouble (sqr_norm b))
+  where sqr_norm (Complex a b) = a * a + b * b
 
 conj :: Complex -> Complex
-conj = undefined
+conj (Complex a b) = Complex a (-b)
 
 -- tests
 
@@ -49,19 +51,33 @@ data Tree a = Node { value :: a, children :: [Tree a] }
 
 -- (a) Возвращает высоту дерева
 height :: Tree a -> Int
-height = undefined
+height (Node v children) = 1 + (max_height children)
+  where
+    max_height [] = 0 
+    max_height (x:xs) = max (height x) (max_height xs)
 
 -- (b) Возвращает среднее арифметическое значений во всех узлах дерева
 -- Необходимо вычислить эту функцию, выполнив один проход по дереву
 avg :: Tree Int -> Int
-avg = undefined
+avg t = x `div` y
+    where (x, y) = avgRec t
+          avgRec (Node v children) = (v + s, 1 + c) where (s, c) = avgRecChildrens children
+          avgRecChildrens [] = (0, 0)
+          avgRecChildrens (x:xs) = (s1 + s2, c1 + c2) where 
+            (s1, c1) = avgRec x
+            (s2, c2) = avgRecChildrens xs
 
 -- (c) Возвращает ширину дерева
 -- Ширина дерева определяется следующим образом:
 -- Количество вершин на определенном уровне называется шириной уровня.
 -- Ширина дерева - это максимальная ширина уровня по всем уровням.
 width :: Tree a -> Int
-width = undefined
+width (Node v children) = max 1 (widthRec children)
+  where
+    widthRec [] = 0
+    widthRec arr = max (length arr) (widthRec (next_level arr))
+    next_level [] = []
+    next_level ((Node v children):xs) = children ++ (next_level xs)
 
 -- tests
 
@@ -104,7 +120,7 @@ width = undefined
 -- 3
 
 data Value = I Int | B Bool deriving (Eq, Show)
-data BinOp = Plus | Mul | Minus | Less | Greater | Equals
+data BinOp = Plus | Mul | Minus | Less | Greater | Equals deriving Show
 data UnOp = Neg | Not
 data Expr = BinOp BinOp Expr Expr | UnOp UnOp Expr | Const Value | If Expr Expr Expr | Var String
 data Statement = Assign String Expr | While Expr Statement | Compound [Statement]
@@ -126,11 +142,58 @@ type Error = String
 -- Ошибки бывают двух видов: необъявленная переменная и несоответствие типов.
 -- Возвращается список ошибок, т.к. выражение может содержать больше одной ошибки.
 evalExpr :: M.Map String Value -> Expr -> Either [Error] Value
-evalExpr = undefined
+evalExpr vars (BinOp operation l r) = case (evalExpr vars l, evalExpr vars r) of
+  (Left x, Left y) -> Left (x ++ y)
+  (Left x, _) -> Left x
+  (_, Left x) -> Left x
+  (Right (I l_int_val), Right (I r_int_val)) -> case operation of
+    Plus -> Right (I (l_int_val + r_int_val))
+    Mul -> Right (I (l_int_val * r_int_val))
+    Minus -> Right (I (l_int_val - r_int_val))
+    Less -> Right (B (l_int_val < r_int_val))
+    Greater -> Right (B (l_int_val > r_int_val))
+    Equals -> Right (B (l_int_val == r_int_val))
+  (Right (B l_bool_val), Right (B r_bool_val)) -> case operation of
+    Equals -> Right (B (l_bool_val == r_bool_val))
+    otherwise -> Left ["Operation " ++ (show operation) ++ " can't be applyed to Bools"]
+  otherwise -> Left ["There is no operation for different types"]
+
+evalExpr vars (UnOp Neg operand) = case evalExpr vars operand of
+  (Left x) -> Left x
+  (Right (I i)) -> Right (I (-i))
+  otherwise -> Left ["Invalid operand type for (Neg). Should be (I Int)"]
+
+evalExpr vars (UnOp Not operand) = case evalExpr vars operand of
+  (Left x) -> Left x
+  (Right (B b)) -> Right (B (not b))
+  otherwise -> Left ["Invalid operand type for (Not). Should be (B Bool)"]
+
+evalExpr vars (Const v) = Right v
+
+evalExpr vars (If state if_true if_false) = case (evalExpr vars state) of
+  (Left x) -> Left x
+  (Right (B True)) -> evalExpr vars if_true
+  (Right (B False)) -> evalExpr vars if_false
+  otherwise -> Left ["If statement has invalid type. Should be (B Bool)"]
+evalExpr vars (Var name) | M.member name vars = Right (vars M.! name)
+                         | otherwise = Left ["Variable " ++ name ++ " not found"]
 
 -- evalStatement принимает текущее значение переменных и statement и возвращает новое значение переменных после его выполнения.
 evalStatement :: M.Map String Value -> Statement -> Either [Error] (M.Map String Value)
-evalStatement = undefined
+evalStatement vars (Assign name expr) = case (evalExpr vars expr) of
+  (Left x) -> Left x
+  (Right value) -> Right (M.insert name value vars)
+evalStatement vars (While state body) = case (evalExpr vars state) of
+  (Left x) -> Left x
+  (Right (B True)) -> case (evalStatement vars body) of
+    (Left x) -> Left x
+    (Right new_vars) -> evalStatement new_vars (While state body)
+  (Right (B False)) -> Right vars
+  (Right (I _)) -> Left ["While statement should have Bool type"]
+evalStatement vars (Compound []) = Right vars
+evalStatement vars (Compound (x:xs)) = case evalStatement vars x of
+  (Left x) -> (Left x)
+  (Right new_vars) -> evalStatement new_vars (Compound xs)
 
 -- tests
 
@@ -163,25 +226,45 @@ testsExpr = [ errorsCount (evalExpr M.empty expr1) ~?= 1
 ------------------------------------------------------------------------------
 -- 4. Реализовать двоичное дерево поиска без балансировки.
 
-data Map k v = Leaf | Branch k v (Map k v) (Map k v)
+-- Я не знаю, было ли так задумано, что Leaf это вроде NullPtr а не лист
+-- но я исходил из этого
+data Map k v = Leaf | Branch k v (Map k v) (Map k v) deriving (Show, Eq)
 
 lookup :: Ord k => k -> Map k v -> Maybe v
-lookup = undefined
+lookup key Leaf = Nothing
+lookup key (Branch k v m_left m_right) | key == k = Just v
+                                       | key < k = lookup key m_left
+                                       | key > k = lookup key m_right
 
 insert :: Ord k => k -> v -> Map k v -> (Map k v, Maybe v)
-insert = undefined
+insert k v Leaf = (Branch k v Leaf Leaf, Nothing)
+insert key value (Branch k v m_left m_right) | key == k = ((Branch k value m_left m_right), Just v)
+                                         | key < k = let (new_left, result) = insert key value m_left 
+                                           in ((Branch k v new_left m_right), result)
+                                         | key > k = let (new_right, result) = insert key value m_right
+                                           in ((Branch k v m_left new_right), result)
 
-delete :: Ord k => k -> Map k v -> Maybe (Map k v)
-delete = undefined
+-- С учетом того, что Leaf == NullPtr Maybe тут не нужно
+--delete :: Ord k => k -> Map k v -> Maybe (Map k v)
+delete :: Ord k => k -> Map k v -> Map k v
+delete _ Leaf = Leaf
+delete key (Branch k v m_left m_right) | key == k = let merge Leaf r = r
+                                                        merge (Branch k v m_left m_right) r = Branch k v m_left (merge m_right r)
+                                                        in merge m_left m_right
+                                       | key < k = Branch k v (delete key m_left) m_right
+                                       | key > k = Branch k v m_left (delete key m_right)
 
 fromList :: Ord k => [(k, v)] -> Map k v
-fromList = undefined
+fromList [] = Leaf
+fromList ((k, v):xs) = fst $ insert k v (fromList xs)
 
 toList :: Map k v -> [(k, v)]
-toList = undefined
+toList Leaf = []
+toList (Branch k v left right) = (toList left) ++ ((k, v):(toList right))
 
 -- tests
 
+-- Функция работает неправильно. Она удаляет одинаковые элементы
 sort :: Ord a => [a] -> [a]
 sort = map fst . toList . fromList . map (\x -> (x, ()))
 
@@ -195,7 +278,11 @@ main = fmap (\_ -> ()) $ runTestTT $ test
       ++ label "width" testsWidth
       ++ label "Expr" testsExpr
       ++ label "Map" -- можете сами написать тесты на каждую функцию :)
-            [ sort [10,24,13,56,35,13,6,23] ~?= [6,10,13,23,24,35,56] ]
+            [ sort [10,24,13,56,35,13,6,23] ~?= [6,10,13,23,24,35,56],
+             delete 1 (fst  (insert 1 0 Leaf)) ~?= Leaf,
+             lookup 1 (fromList [(1, 2), (2, 4), (0, 0), ((-5), (-10))]) ~?= Just 2,
+             lookup 2 (fromList [(1, 2), (2, 4), (0, 0), ((-5), (-10))]) ~?= Just 4,
+             lookup (-5) (fromList [(1, 2), (2, 4), (0, 0), ((-5), (-10))]) ~?= Just (-10)]
   where
     label :: String -> [Test] -> [Test]
     label l = map (\(i,t) -> TestLabel (l ++ " [" ++ show i ++ "]") t) . zip [1..]
