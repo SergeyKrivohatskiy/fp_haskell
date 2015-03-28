9a7350965d3855e3c64f179f6e7d4fc01f779120
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-03-28 22:50:39 +0300

    hw07 Complex and Tree finished
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- hw07/Complex.hs
+++ hw07/Complex.hs
@@ -5,24 +5,51 @@ module Complex where
 data Complex = Complex { real :: Double, im :: Double } deriving Eq
 
 instance Num Complex where
-    (+) = undefined
-    (*) = undefined
-    fromInteger = undefined
-    negate = undefined
-    abs = undefined
+    (+) (Complex a1 b1) (Complex a2 b2) = Complex (a1 + a2) (b1 + b2)
+    (*) (Complex a1 b1) (Complex a2 b2) = Complex (a1 * a2 - b1 * b2) (a1 * b2 + a2 * b1)
+    fromInteger i = Complex (fromInteger i) 0
+    negate (Complex i j) = (Complex (negate i) (negate j))
+    abs (Complex i j) = (Complex (sqrt (i * i + j * j)) 0)
     
     signum _ = error "Complex: signum isn't defined"
 
 instance Fractional Complex where
-    (/) = undefined
-    fromRational = undefined
+    (/) (Complex a1 b1) (Complex a2 0) = Complex (a1 / a2) (b1 / a2)
+    (/) a b = (a * (conj b)) / (Complex (sqr_norm b) 0)
+      where 
+        sqr_norm (Complex a b) = a * a + b * b
+        conj (Complex a b) = Complex a (-b)
+    fromRational i = Complex (fromRational i) 0
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show Complex where
-    show = undefined
+    show (Complex a b) | b == 0 = show a
+                       | a == 0 = (show b) ++ " * i"
+                       | b > 0 = (show a) ++ " + " ++ (show b) ++ " * i"
+                       | b < 0 = (show a) ++ " - " ++ (show (abs b)) ++ " * i"
+
+starts_with str [] = True
+starts_with [] (_:_) = False
+starts_with (x:xs) (y:ys) = x == y && starts_with xs ys
+
+remove str 0 = str
+remove (x:xs) i = remove xs (i - 1)
+
 
 instance Read Complex where
-    readsPrec = undefined
+    readsPrec unused str = case readsPrec unused str of 
+        [] -> []
+        ((a, remaind):xs) -> if starts_with remaind " * i" 
+            then [(Complex 0 a, remove remaind 4)]
+            else let (b, remain_remaind) = read_b unused remaind in [(Complex a b, remain_remaind)]
+        where 
+            read_b unused str | starts_with str " + " = case readsPrec unused (remove str 3) of 
+                                    [] -> (0, str)
+                                    [(b, remaind)] -> if starts_with remaind " * i" then (b, remove remaind 4) else (0, str)
+                              | starts_with str " - " = case readsPrec unused (remove str 3) of 
+                                    [] -> (0, str)
+                                    [(b, remaind)] -> if starts_with remaind " * i" then ((negate b), remove remaind 4) else (0, str)
+                              | otherwise = (0, str)
 
 i :: Complex
-i = undefined
+i = Complex 0 1
--- hw07/Tree.hs
+++ hw07/Tree.hs
@@ -6,10 +6,42 @@ data Tree a = Node { value :: a, children :: [Tree a] } deriving Eq
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show a => Show (Tree a) where
-    show = undefined
+    show (Node v children) = (show v) ++ (show_children children) where
+        show_children [] = ""
+        show_children (x:xs) = ":{" ++ (show x) ++ (show_other xs) ++ "}"
+        show_other [] = ""
+        show_other (x:xs) = "," ++ (show x) ++ (show_other xs)
 
+starts_with str [] = True
+starts_with [] (_:_) = False
+starts_with (x:xs) (y:ys) = x == y && starts_with xs ys
+
+ends_with str1 str2 = starts_with (reverse str1) (reverse str2)
+
+remove str 0 = str
+remove (x:xs) i = remove xs (i - 1)
+
+remove_back str i = reverse (remove (reverse str) i)
+
+read_children unused str = case (readsPrec unused str) of
+    [] -> Nothing
+    [(child, remaind)] -> if remaind `starts_with` ","
+        then case (read_children unused (remove remaind 1)) of
+            Just (children, remaind_remind) -> Just ((child: children), remaind_remind)
+            Nothing -> Nothing
+        else if remaind `starts_with` "}"
+                then Just ([child], remove remaind 1)
+                else Nothing
 instance Read a => Read (Tree a) where
-    readsPrec = undefined
+    readsPrec unused str = case readsPrec unused str of 
+        [] -> []
+        ((v, remaind):xs) -> (if remaind `starts_with` ":{"
+            then case (read_children unused (remove remaind 2)) of
+                Just (children, remaind_remind) -> [(Node v children, remaind_remind)]
+                Nothing -> [(Node v [], remaind)]
+            else [(Node v [], remaind)])
 
 instance Functor Tree where
-    fmap = undefined
+    fmap f (Node v children) = (Node (f v) (fmap_children f children)) where
+        fmap_children f [] = []
+        fmap_children f (x:xs) = ((fmap f x):(fmap_children f xs))
