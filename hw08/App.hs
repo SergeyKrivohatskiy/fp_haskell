b4df8bab7e9ad4a3273e598dc20bd2173693528c
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-04-08 15:21:44 +0300

    hw08 part 2
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- hw08/App.hs
+++ hw08/App.hs
@@ -5,8 +5,13 @@ import Control.Applicative
 data Tree a = Node a [Tree a]
 
 instance Functor Tree where
-    fmap = undefined
+    fmap f (Node a arr) = Node (f a) (fmapArr f arr) where
+        fmapArr f [] = []
+        fmapArr f (x:xs) = (fmap f x: fmapArr f xs)
+
 
 instance Applicative Tree where
-    pure = undefined
-    (<*>) = undefined
+    pure x = Node x (repeat (pure x))
+    (<*>) (Node ab abArr) (Node a aArr) = Node (ab a) (processArr abArr aArr) where
+        processArr [] _ = []
+        processArr (x:xs) (y:ys) = ((<*>) x y: processArr xs ys)
