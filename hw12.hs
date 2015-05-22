a796561a2938f13801c453d905aebaf560bc380a
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-05-22 22:59:30 +0300

    hw12 finished
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- hw12.hs
+++ hw12.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+import Control.Monad.Identity
 import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Writer
@@ -39,8 +40,45 @@ mkEquasions (expr1 :@ expr2) = do
 
 runMkEquasions expr types = (runWriter (runStateT (runReaderT (mkEquasions expr) types) 0))
 
---Example:
---runMkEquasions (Lam "x" (Var "x" :@ Var "y")) [("y", TVar "yType")]
+-- replace type 1 to type 2 in type 3
+substitution :: Type -> Type -> Type -> Type
+substitution (TVar a) b (TVar t) | a == t = b
+                                 | otherwise = (TVar t)
+substitution a b (t1 :-> t2) = (substitution a b t1) :-> (substitution a b t2)
+
+--Example
+--substitution (TVar "a") ((TVar "b") :-> (TVar "b")) ((TVar "c") :-> (TVar "a"))
+
+--Return unifier for types 1 and 2
+unifier :: Type -> Type -> Either String (Type -> Type)
+unifier (TVar t1) t2 | (TVar t1) == t2 = Right id
+                     | contains t2 t1 = Left ("Unifier do not exists. " ++ (show t2) ++ " contains TVar \"" ++ t1 ++ "\"")
+                     | otherwise = Right $ substitution (TVar t1) t2 where
+                        contains (TVar t) t1 = t == t1
+                        contains (type1 :-> type2) t1 = (contains type1 t1) || (contains type2 t1)
+unifier (t1 :-> t2) (TVar t3) = unifier (TVar t3) (t1 :-> t2)
+--U(σ1→σ2,τ1→τ2) = U(U(σ2,τ2)σ1,U(σ2,τ2)τ1)◦U(σ2,τ2) 
+unifier (o1 :-> o2) (t1 :-> t2) = case unifier o2 t2 of
+    Right uo2t2 -> case (unifier (uo2t2 o1) (uo2t2 t1)) of 
+        Right tmp -> Right (tmp . uo2t2)
+        Left err -> Left err
+    Left err -> Left err
+
+getType' :: Expr -> Identity (Either String Type)
+getType' expr = do
+    tmp <- return $ runMkEquasions expr []
+    mainType <- return $ fst $ fst $ tmp
+    equasions <- return $ snd $ tmp
+    return $ processEquasions mainType equasions where
+        processEquasions mainType [] = Right mainType
+        processEquasions mainType (x:xs) = case unifier (fst x) (snd x) of
+            Right unifier' -> processEquasions (unifier' mainType) (map (\p -> (unifier' $ fst p, unifier' $ snd p)) xs)
+            Left err -> Left err
 
 inferType :: Expr -> Either String Type
-inferType = undefined
+inferType = runIdentity . getType'
+
+--Examples
+--inferType (Lam "x" (Var "x"))
+--inferType (Lam "x" ((Var "x") :@ (Var "x")))
+--inferType (Lam "x" (Lam "y" ((Var "y") :@ (Var "x"))))
