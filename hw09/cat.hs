f2a282bf2d5e6481f95fcf093ae8e483e17cfb5b
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-05-23 14:53:58 +0300

    hw09 cat
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- hw09/cat.hs
+++ hw09/cat.hs
@@ -1,3 +1,6 @@
+import System.IO
+import System.Environment
+import Control.Monad
 import Control.Exception(catch)
 
 {-
@@ -7,5 +10,22 @@ cat принимает имена файлов и выводит их содер
 (1.5 балла)
 -}
 
+printStdin :: IO ()
+printStdin = do
+    hSetBuffering stdin LineBuffering
+    hSetBuffering stdout LineBuffering
+    forever (getLine >>= putStrLn)
+
 main :: IO ()
-main = undefined
+main = do fileNames <- getArgs
+          if null fileNames then
+            printStdin
+          else
+            printFiles fileNames where
+                printFiles [] = return ()
+                printFiles (fileName: fileNames) = do
+                    catch (readFile fileName) exHandler >>= putStr 
+                    printFiles fileNames
+                exHandler :: IOError -> IO String
+                exHandler e = putStr (show e) >> return ""
+
